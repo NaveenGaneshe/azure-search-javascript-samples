c36ecfd52e0041cdef7d7617fe80e2bbb7c940bb
Naveen <n.ganeshe@yahoo.com>
2024-04-04 15:20:40 -0400

    Added '@azure/storage-blob'

--- search-website-functions-v4/client/src/components/BookDetail/SearchPage/simple.js
+++ search-website-functions-v4/client/src/components/BookDetail/SearchPage/simple.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'; 
+
 import CardMedia from "@mui/material/CardMedia";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
