7672fd8e4e236c82eec1204b0a082b0af8f67e4a
Naveen <n.ganeshe@yahoo.com>
2024-04-09 01:06:39 +0530

    v3

--- search-website-functions-v4/client/src/components/BookDetail/SearchPage/simple.js
+++ search-website-functions-v4/client/src/components/BookDetail/SearchPage/simple.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import CardMedia from "@mui/material/CardMedia";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -8,19 +8,12 @@ import styled from "@emotion/styled";
 import { BlobServiceClient } from '@azure/storage-blob';
 
 const StyledCard = styled(Card)`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-`;
-
-const StyledCardContent = styled(CardContent)`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledIframeContainer = styled.div`
-  flex: 1;
+  width: 10rem;
+  padding: 16px;
+  text-align: center;
+  margin: 10px;
+  inline-block;
+  max-height: 18rem;
 `;
 
 const StyledCardActionArea = styled(CardActionArea)`
@@ -50,6 +43,13 @@ export default function BookCardSimple({ document }) {
         return title;
     };
 
+    const decodeBase64 = (input) => {
+        var atob = require('atob');
+        var output = atob(input);
+        return output;
+    }
+
+    const [blobContent, setBlobContent] = useState('');
     const [iframeSrc, setIframeSrc] = useState('');
     useEffect(() => {
         const blobSasUrl = "https://ecftranslatorapp.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-04-18T05:41:52Z&st=2024-04-01T21:41:52Z&spr=https&sig=f0pbuGqjDY84WFsCbEUcuG1iReUWe2Zxsh7GccFBxpY%3D";
@@ -59,6 +59,11 @@ export default function BookCardSimple({ document }) {
 
         const containerName = "notes-source-ai-files";
         const blobName = document.metadata_storage_name;
+
+        //// Get a container client from the BlobServiceClient
+        //const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        //const blobClient = containerClient.getBlobClient(blobName);
 
         async function fetchBlobContent(inputBase64) {
             const containerClient = blobServiceClient.getContainerClient(containerName);
@@ -70,6 +75,18 @@ export default function BookCardSimple({ document }) {
             return URL.createObjectURL(blob); // Pass the Blob object to createObjectURL
         }
 
+        // Function to convert blob to string
+        async function blobToString(blob) {
+            const fileReader = new FileReader();
+            return new Promise((resolve, reject) => {
+                fileReader.onloadend = (ev) => {
+                    resolve(ev.target.result);
+                };
+                fileReader.onerror = reject;
+                fileReader.readAsDataURL(blob);
+            });
+        }
+
         fetchBlobContent(blobName).then((blobContentUrl) => {
             setIframeSrc(blobContentUrl);
         });
@@ -77,26 +94,25 @@ export default function BookCardSimple({ document }) {
 
     return (
         <StyledCard>
-            <StyledCardContent>
+            <StyledCardActionArea href={`/details/${document.id}`}>
                 <StyledCardContentImage>
                     {/*<StyledImg*/}
                     {/*    // image={document.image_url}*/}
                     {/*    title={document.metadata_title}*/}
                     {/*    alt={document.metadata_storage_file_extension}*/}
                     {/*/>*/}
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        {shortenTitle(blobName)}
-                        <br />
-                     {/*   Image Path: {blobContent}*/}
-                    </Typography>
+
+                    <iframe src={iframeSrc}></iframe>
                 </StyledCardContentImage>
-            </StyledCardContent>
-            <StyledIframeContainer>
-                <iframe src={iframeSrc}></iframe>
-            </StyledIframeContainer>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {shortenTitle(document.metadata_title)}
+                        <br />
+                        Image Path: {blobContent}
+                    </Typography>
+                </CardContent>
+                {/*<iframe src={decodeBase64(document.metadata_storage_path)}></iframe>*/}
+            </StyledCardActionArea>
         </StyledCard>
     );
 }
-
-
-
